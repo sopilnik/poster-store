7f2378fb43e29d21ed5b1868bce632560c66175d
Alexandr Sopilnik <sopilnik.dev@gmail.com>
2026-09-15 18:46:45 +0500

    Use next/link on the not-found page so lint passes

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,11 @@
+import Link from 'next/link'
+
 export default function NotFound() {
   return (
     <main>
       <h1>Page not found</h1>
       <p>
-        <a href="/">Home</a>
+        <Link href="/">Home</Link>
       </p>
     </main>
   )
